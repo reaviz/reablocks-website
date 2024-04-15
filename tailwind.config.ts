dcc46e66fb4614ba0eb763c0ad98fe3048ee2967
Grant Steffen <gsteffen1203@gmail.com>
2024-04-15 11:49:20 -0500

    Add a hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,31 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: { DEFAULT: "7.5rem", sm: "2rem", lg: "7.5rem" },
+      screens: {
+        "2xl": "1440px",
+      },
+    },
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#105EFF",
+          hover: "#4C86FF",
+        },
+        content: {
+          primary: "#F2F3F7",
+          secondary: "#C6CBD9",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        button: "0px 4px 16px 0px rgba(0, 0, 0, 0.25)",
       },
     },
   },
