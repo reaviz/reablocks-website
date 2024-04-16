5d1456029913c2ec149469f77f098f761e7148f3
Grant Steffen <gsteffen1203@gmail.com>
2024-04-16 13:01:03 -0500

    Pull sparkles into icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
