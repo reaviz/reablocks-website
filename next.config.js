7f1b4177cb2c227e20471ef687e27b4f67afcf6b
amcdnl <amcdaniel2@gmail.com>
2024-04-30 04:22:01 -0400

    next docs

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx'
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  /*
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
+    });
+
+    return config;
+  },
+  */
+};
+
+module.exports = withNextra(nextConfig);
---
+++ theme.config.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  logo: (
+    <img
+      style={{ width: 150 }}
+      src="https://github.com/reaviz/reablocks/raw/master/docs/assets/logo.png"
+    />
+  ),
+  project: {
+    link: 'https://github.com/reaviz/reablocks'
+  }
+};
