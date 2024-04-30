706d8d1f50287c19d7cd81aea26d1dcc1e043d05
amcdnl <amcdaniel2@gmail.com>
2024-04-30 06:55:46 -0400

    moar updates

--- next.config.js
+++ next.config.js
@@ -6,16 +6,6 @@ const withNextra = require('nextra')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /*
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-    });
-
-    return config;
-  },
-  */
 };
 
 module.exports = withNextra(nextConfig);
--- theme.config.tsx
+++ theme.config.tsx
@@ -3,10 +3,31 @@ export default {
   logo: (
     <img
       style={{ width: 150 }}
+      alt="Logo"
       src="https://github.com/reaviz/reablocks/raw/master/docs/assets/logo.png"
     />
   ),
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  // hide the button
+  darkMode: false,
+  footer: {
+    text: (
+      <span>
+        Apache {new Date().getFullYear()} ©{' '}
+        <a href="https://goodcode.us" target="_blank">
+          Good Code
+        </a>
+        .
+      </span>
+    )
+  },
+  feedback: {
+    content: null
+  },
   project: {
     link: 'https://github.com/reaviz/reablocks'
-  }
+  },
+  docsRepositoryBase: 'https://github.com/reaviz/reablocks/tree/master/docs'
 };
