689079e6de7a1f834a6ac793377809d462abb2a5
amcdnl <amcdaniel2@gmail.com>
2024-05-13 06:04:09 -0400

    storybook no workie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { ExternalDocs } from '@storybook/blocks';
+import * as reactAnnotations from '@storybook/react/dist/entry-preview.mjs';
+import * as previewAnnotations from '../../.storybook/preview';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ExternalDocs projectAnnotationsList={[reactAnnotations, previewAnnotations]}>
+      <Component {...pageProps} />
+    </ExternalDocs>
+  );
 }
