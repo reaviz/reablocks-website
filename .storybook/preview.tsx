df3ecbe844c74f3daa31e70a1cc70da9ded3ea11
amcdnl <amcdaniel2@gmail.com>
2024-05-13 13:25:11 -0400

    fix preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,16 @@
 import type { Preview } from "@storybook/react";
+import { ThemeProvider, theme } from 'reablocks';
+
+const withProvider = (Story, context) => (
+  <ThemeProvider theme={theme}>
+    <Story {...context} />
+  </ThemeProvider>
+);
 
 const preview: Preview = {
+  decorators: [
+    withProvider
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
