381b5c882d437d02d094ec548c4d259352e6ed5a
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2024-05-13 18:02:22 +0300

    add DateFormat doc [broken]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { ExternalDocs } from '@storybook/blocks';
-import * as reactAnnotations from '@storybook/react/dist/entry-preview.mjs';
-import * as previewAnnotations from '../../.storybook/preview';
+import { ExternalDocs } from "@storybook/blocks";
+import { ThemeProvider, theme } from "reablocks";
+import * as reactAnnotations from "@storybook/react/dist/entry-preview.mjs";
+import * as previewAnnotations from "../../.storybook/preview";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ExternalDocs projectAnnotationsList={[reactAnnotations, previewAnnotations]}>
-      <Component {...pageProps} />
-    </ExternalDocs>
+    <ThemeProvider theme={theme}>
+      <ExternalDocs
+        projectAnnotationsList={[reactAnnotations, previewAnnotations]}
+      >
+        <Component {...pageProps} />
+      </ExternalDocs>
+    </ThemeProvider>
   );
 }
