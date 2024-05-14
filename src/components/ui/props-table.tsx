cc1c8280609e3d07f58782f2f50325e86146276c
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2024-05-14 14:24:51 +0300

    doc migration

--- src/components/ui/props-table.tsx
+++ src/components/ui/props-table.tsx
@@ -1,41 +1,67 @@
-import { Unstyled } from '@storybook/blocks';
-import metadata from 'reablocks/stories/docs.json';
+import { Unstyled } from "@storybook/blocks";
+import metadata from "reablocks/stories/docs.json";
 
 export const PropsTable = ({ name }) => {
-  const data: any = metadata.find(m => m.displayName === name);
-  const keys = Object.keys(data.props);
+  const data: any = metadata.find((m) => m.displayName === name);
+  const keys = Object.keys(data?.props ?? {});
 
   return (
     <Unstyled>
-      <table style={{ width: '100%', fontSize: 13 }}>
+      <table style={{ width: "100%", fontSize: 13 }}>
         <thead>
           <tr>
-            <th style={{ padding: '5px 0', textAlign: 'left', borderBottom: '1px solid hsla(203, 50%, 30%, 0.15)' }}>Prop</th>
-            <th style={{ padding: '5px 0', textAlign: 'left', borderBottom: '1px solid hsla(203, 50%, 30%, 0.15)' }}>Description</th>
-            <th style={{ padding: '5px 0', textAlign: 'left', borderBottom: '1px solid hsla(203, 50%, 30%, 0.15)' }}>Default</th>
+            <th
+              style={{
+                padding: "5px 0",
+                textAlign: "left",
+                borderBottom: "1px solid hsla(203, 50%, 30%, 0.15)",
+              }}
+            >
+              Prop
+            </th>
+            <th
+              style={{
+                padding: "5px 0",
+                textAlign: "left",
+                borderBottom: "1px solid hsla(203, 50%, 30%, 0.15)",
+              }}
+            >
+              Description
+            </th>
+            <th
+              style={{
+                padding: "5px 0",
+                textAlign: "left",
+                borderBottom: "1px solid hsla(203, 50%, 30%, 0.15)",
+              }}
+            >
+              Default
+            </th>
           </tr>
         </thead>
         <tbody>
-          {keys.map(prop => (
-            <tr key={prop} >
-              <td style={{ padding: '5px 0' }}>
+          {keys.map((prop) => (
+            <tr key={prop}>
+              <td style={{ padding: "5px 0" }}>
                 <code>{prop}</code>
                 {data.props[prop].required && <i> *</i>}
               </td>
-              <td style={{ padding: '5px 0' }}>
+              <td style={{ padding: "5px 0" }}>
                 {data.props[prop].description}
                 {data.props[prop].tsType && (
                   <>
                     <br />
                     <code>
-                      {data.props[prop].tsType.raw ?? data.props[prop].tsType.name}
+                      {data.props[prop].tsType.raw ??
+                        data.props[prop].tsType.name}
                     </code>
                   </>
                 )}
               </td>
-              <td style={{ padding: '5px 0' }}>
+              <td style={{ padding: "5px 0" }}>
                 <code>
-                  {data.props[prop].defaultValue && data.props[prop].defaultValue.value}
+                  {data.props[prop].defaultValue &&
+                    data.props[prop].defaultValue.value}
                 </code>
               </td>
             </tr>
