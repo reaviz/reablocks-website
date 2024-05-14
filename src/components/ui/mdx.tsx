c7e1eeac24b670c9db104ec3053e9467c7c46d37
amcdnl <amcdaniel2@gmail.com>
2024-05-14 06:59:57 -0400

    shutup

--- src/components/ui/mdx.tsx
+++ src/components/ui/mdx.tsx
@@ -3,14 +3,14 @@ import * as provider from "@mdx-js/react";
 import { useMDXComponents } from "nextra/mdx";
 import * as runtime from "react/jsx-runtime";
 
-export default function Mdx({ children }) {
+export default function Mdx({ children }: any) {
   const { default: MDXContent } = evaluateSync(children, {
     ...runtime,
     ...provider,
     useMDXComponents,
     format: "mdx",
     development: false,
-  });
+  } as any);
 
   return <MDXContent />;
 }
