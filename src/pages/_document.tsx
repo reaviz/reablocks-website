a7ead3b3349e504a4f8c139537940cdaefd63800
amcdnl <amcdaniel2@gmail.com>
2024-05-15 11:06:07 -0400

    posthog hook

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,16 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import posthog from 'posthog-js';
+import { useEffect } from 'react';
 
 export default function Document() {
+  useEffect(() => {
+    try {
+      posthog.init('phc_KpMeH726m3pXXzMzj5AuiY1SDPstX8pTVxmxPEJkiCK', { api_host: "https://us.i.posthog.com" })
+    } catch {
+      /** noop */
+    }
+  });
+
   return (
     <Html lang="en">
       <Head>
