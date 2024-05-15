a2ad3655c484399da17403a5535de2810bf65f75
amcdnl <amcdaniel2@gmail.com>
2024-05-15 11:23:47 -0400

    fix analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,18 @@ import { ThemeProvider, theme } from 'reablocks';
 import { ExternalDocs } from '@storybook/blocks';
 import * as reactAnnotations from '@storybook/react/dist/entry-preview.mjs';
 import * as previewAnnotations from '../../.storybook/preview';
+import { useEffect } from 'react';
+import posthog from 'posthog-js';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    try {
+      posthog.init('phc_KpMeH726m3pXXzMzj5AuiY1SDPstX8pTVxmxPEJkiCK', { api_host: "https://us.i.posthog.com" })
+    } catch {
+      /** noop */
+    }
+  }, []);
+  
   if (Component.name === 'Home') {
     return (
       <Unstyled>
