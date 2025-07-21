cce389419304adea9ba099d2b79a0527b909c01e
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-07-21 13:42:24 +0300

    fix footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 const footer = (
   <Footer className="flex w-full justify-center py-4">
     <LandingFooter
-      logo={<LogoIcon className="h-fit w-[150px] text-white" />}
+      logo={<LogoIcon className="h-fit w-[150px] text-[var(--foreground)]" />}
       className="py-6 text-base"
       libName="reablocks"
     />
