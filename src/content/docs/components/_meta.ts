5f7df2119497f6eebe1969aa1862e2e51d5d90bf
Serhii.Tsybulskyi <c4r0n0s@gmail.com>
2025-07-22 17:25:32 +0300

    expand all components categories

--- src/content/docs/components/_meta.ts
+++ src/content/docs/components/_meta.ts
@@ -1,10 +1,10 @@
 const meta = {
-  "data": "Data",
-  "elements": "Elements",
-  "form": "Form",
-  "layers": "Layers",
-  "layout": "Layout",
-  "typography": "Typography"
-}
+  data: { title: 'Data', theme: { collapsed: false } },
+  elements: { title: 'Elements', theme: { collapsed: false } },
+  form: { title: 'Form', theme: { collapsed: false } },
+  layers: { title: 'Layers', theme: { collapsed: false } },
+  layout: { title: 'Layout', theme: { collapsed: false } },
+  typography: { title: 'Typography', theme: { collapsed: false } }
+};
 
 export default meta;
