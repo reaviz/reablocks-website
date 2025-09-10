acab3756e83be5ba2b076ba0281ec412b9316346
Evgeniy <evgeniy@goodcode.us>
2025-09-10 15:46:10 +0300

    reablocks-docs-theme version update

--- src/app/api/story-source/route.ts
+++ src/app/api/story-source/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest } from "next/server";
-import { getStorySource } from 'reablocks-docs-theme'
+import { getStorySource } from 'reablocks-docs-theme';
+import { join } from 'path';
 
 export async function GET (request: NextRequest) {
-  return getStorySource(request);
+  const { searchParams } = new URL(request.url);
+  const storyPath = searchParams.get('storyPath') ?? '';
+  const functionName = searchParams.get('functionName') ?? '';
+  const filePath = join(process.cwd(), 'src', 'stories', storyPath);
+
+  return getStorySource(filePath, functionName);
 }
