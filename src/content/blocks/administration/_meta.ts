803a93426fcc48ab5b97aab29a25451fdaccd9f9
amcdnl <amcdaniel2@gmail.com>
2026-01-08 07:16:20 -0500

    remove new tags

--- src/content/blocks/administration/_meta.ts
+++ src/content/blocks/administration/_meta.ts
@@ -10,7 +10,7 @@ const meta = {
     },
   },
   profile: {
-    title: "Profile 🆕",
+    title: "Profile",
     theme: {
       layout: "full",
     },
--- src/content/blocks/foundation/_meta.ts
+++ src/content/blocks/foundation/_meta.ts
@@ -15,7 +15,7 @@ const meta = {
     },
   },
   timeline: {
-    title: "Timeline 🆕",
+    title: "Timeline",
     theme: {
       layout: "full",
     },
