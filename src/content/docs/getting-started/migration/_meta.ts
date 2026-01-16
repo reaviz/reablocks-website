ec9e8389f8434d6eea9eec3b488a819fa28e5230
erin bassity <erin@goodcode.us>
2026-01-16 15:36:45 -0700

    update docs

--- src/content/docs/getting-started/migration/_meta.ts
+++ src/content/docs/getting-started/migration/_meta.ts
@@ -4,6 +4,7 @@ const meta = {
   'from-v7-to-v8': 'From v7 to v8',
   'from-v8-to-v9': 'From v8 to v9',
   'from-v9-to-v10': 'From v9 to v10',
+  'default-to-unify-token-mapping': 'Default to Unify Token Mapping',
 };
 
 export default meta;
